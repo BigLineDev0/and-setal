0dd55a13a28cbba3cbc3ad50ed195dfaa7045bac
Fatoumata DRAME <dramefatoumata029@gmail.com>
2026-08-19 14:06:54 +0000

    Ajout du champ adresse dans le modele Incident

--- incident/serializers.py
+++ incident/serializers.py
@@ -1,5 +1,7 @@
 
 from rest_framework import serializers
+
+from incident.utilis import reverse_geocode
 from .models import Incident
 
 
@@ -9,8 +11,8 @@ class IncidentSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Incident
-        fields = ['id','statut', 'description', 'messageVocal', 'longitude', 'latitude', 'dateCreation', 'dateModification', 'priorite', 'urlImage', 'type_incident', 'citoyen']
-        read_only_fields = ['id', 'statut', 'dateCreation', 'dateModification', 'priorite', 'type_incident', 'citoyen']
+        fields = ['id','statut', 'description', 'messageVocal', 'longitude', 'latitude', 'adresse','dateCreation', 'dateModification', 'priorite', 'urlImage', 'type_incident', 'citoyen']
+        read_only_fields = ['id', 'statut', 'dateCreation', 'dateModification', 'priorite', 'type_incident', 'citoyen', 'adresse']  # Champs en lecture seule
 
 
 
@@ -20,6 +22,7 @@ class IncidentSerializer(serializers.ModelSerializer):
         analyse = obj.analyseia_set.first()  #  first() pour obtenir le premier objet AnalyseIA associé à l'incident ou .get() avec try/except
         return analyse.type_incident if analyse else None
 
+    # Récupère les informations du citoyen associé à l'incident
     def get_citoyen(self, obj):
         if obj.citoyen:
             return {
@@ -29,3 +32,16 @@ class IncidentSerializer(serializers.ModelSerializer):
                 "email": obj.citoyen.email,
             }
         return None  # signalement anonyme
+
+    # Lors de la création d'un incident, on peut utiliser la fonction reverse_geocode pour remplir automatiquement le champ adresse à partir des coordonnées GPS fournies.
+    def create(self, validated_data):
+        latitude = validated_data.get('latitude')
+        longitude = validated_data.get('longitude')
+
+        if latitude is not None and longitude is not None:
+            validated_data['adresse'] = reverse_geocode(
+                latitude,
+                longitude
+            )
+
+        return super().create(validated_data)
