a915e1c041893d9a3fc17b5011d2c9243e4a95e0
Fatoumata DRAME <dramefatoumata029@gmail.com>
2026-08-20 18:01:09 +0000

    Ajout du nom de l'agent dans le serializer de l'intervention

--- interventions/serializers.py
+++ interventions/serializers.py
@@ -3,8 +3,18 @@ from .models import Intervention
 
 
 class InterventionSerializer(serializers.ModelSerializer):
+    agent = serializers.SerializerMethodField()
+
     class Meta:
         model = Intervention
         fields = ['id', 'signalement', 'agent', 'statut', 'photo', 'commentaire', 'date_creation', 'date_fin']
-        read_only_fields = ['id', 'date_creation']
-        
+        read_only_fields = ['id', 'date_creation', 'agent']
+
+
+    def get_agent(self, obj):
+        if obj.agent:
+            return {
+                "prenom": obj.agent.first_name,
+                "nom": obj.agent.last_name
+            }
+        return None
